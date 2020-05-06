0665d706da7a42c5aa5c3f7c45179a4594ff3e57
Ravi Rao <ravi.rao04@sap.com>
2020-05-06 19:06:51 +0530

    Rearranged the file

---
+++ UDPServer/src/app/ServerApp.java
@@ -0,0 +1,21 @@
+package app;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class ServerApp {
+    public static void main(String args[]) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        int portNumber;
+        String fileName;
+
+        if (args.length != 2) {
+            System.out.println("The total number of arguments required are 2");
+            System.out.println("The arguments are portNumber and fileName");
+        } else {
+            portNumber = Integer.parseInt(args[0]);
+            fileName = args[1];
+            Server server = Server.getInstance(portNumber, fileName);
+            server.receiveData(fileName);
+        }
+    }
+}
