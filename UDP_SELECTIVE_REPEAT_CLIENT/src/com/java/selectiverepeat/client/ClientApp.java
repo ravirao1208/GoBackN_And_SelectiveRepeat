a943668541c8443e18688b61503e4c05c0191631
Ravi Rao <ravi.rao04@sap.com>
2020-05-27 20:08:20 +0530

    add comm

--- UDP_SELECTIVE_REPEAT_CLIENT/src/com/java/selectiverepeat/client/ClientApp.java
+++ UDP_SELECTIVE_REPEAT_CLIENT/src/com/java/selectiverepeat/client/ClientApp.java
@@ -54,16 +54,24 @@ public class ClientApp {
 				ds = new DatagramSocket();
 				while (true) {
 					long currentTime = System.currentTimeMillis();
+					// checking for time-out (Packet)
+					// if contains CurrentTime or past-Time and and its associated packet-sqe-no has
+					// no ack
 					if (SRRPACKETSENDTIME.size() > 0 && SRRPACKETSENDTIME.containsKey(System.currentTimeMillis())
 							&& (!ACKNOWLEDGEDPACKETLIST.contains(SRRPACKETSENDTIME.get(System.currentTimeMillis())))) {
+						// get the packet that is associated to current time which hasn't got ack
 						int packetIDToBeResend = SRRPACKETSENDTIME.get(System.currentTimeMillis());
+						// put it to bucket
 						SRRPACKETSENDTIME.put(currentTime + 400, packetIDToBeResend);
-						byte[] udpPacket = ClientHelper.finalPacketFrames(byteArray2[packetIDToBeResend], MSS, packetIDToBeResend);
+						// get the packet that has to be re-sent
+						byte[] udpPacket = ClientHelper.finalPacketFrames(byteArray2[packetIDToBeResend], MSS,
+								packetIDToBeResend);
 						DatagramPacket dp = new DatagramPacket(udpPacket, udpPacket.length, InetAddress.getLocalHost(),
 								serverPortNumber);
 						ds.send(dp);
-						 System.out.println("Packet Loss Timeout: "+ packetIDToBeResend); 
+						System.out.println("Packet Loss Timeout: " + packetIDToBeResend);
 					}
+					// Condition to stop
 					if (ACKNOWLEDGEDPACKETLIST.size() == windowSize) {
 						windowFilled = Boolean.TRUE;
 						ACKNOWLEDGEDPACKETLIST.clear();
@@ -77,15 +85,21 @@ public class ClientApp {
 
 	public static void SRRSendPacketToServer(String hostName, int serverPort, String fileName, int windowSize, int MSS)
 			throws IOException, InterruptedException {
-		Path path = Paths.get(System.getProperty("java.class.path") + "\\..\\resources\\clientfiles\\" + fileName + ".txt");
+		Path path = Paths
+				.get(System.getProperty("java.class.path") + "\\..\\resources\\clientfiles\\" + fileName + ".txt");
 		byteArray = Files.readAllBytes(path);
 		byteArray2 = ClientHelper.chunkArray(byteArray, MSS);
 		InetAddress serverAdress = InetAddress.getByName(hostName);
+		// eg 5< 100
 		while (CURRENTWINDOWPOINTER < byteArray2.length - 1) {
-			if (CURRENTACKNOWLEDGEDPACKETNUMBER <= tempWindowPointer /* && windowFilled.equals(Boolean.TRUE) */) {
+			// init 0 == 0
+			// 0 <= 4 || 4<=4 || 3<=4
+			if (CURRENTACKNOWLEDGEDPACKETNUMBER <= tempWindowPointer) {
 				windowFilled = Boolean.FALSE;
 				tempWindowPointer = windowSize + tempWindowPointer;
-				ClientHelper.selectiveRepeatRequest(CURRENTWINDOWPOINTER, windowSize, byteArray2, ds, MSS, serverAdress, serverPort);
+				// eg :: 0,5,data[], ClientSocket, MSS, host ,port
+				ClientHelper.selectiveRepeatRequest(CURRENTWINDOWPOINTER, windowSize, byteArray2, ds, MSS, serverAdress,
+						serverPort);
 			}
 		}
 	}
@@ -99,15 +113,16 @@ public class ClientApp {
 					DatagramPacket receivedDatagram = new DatagramPacket(sequenceNumberBytes,
 							sequenceNumberBytes.length);
 					ds.receive(receivedDatagram);
+					// currently rev-packet seq (Max) eg 4,5 == 5
 					CURRENTACKNOWLEDGEDPACKETNUMBER = Math.max(new BigInteger(sequenceNumberBytes).intValue(),
 							CURRENTACKNOWLEDGEDPACKETNUMBER);
+					// keeping track of ack-rec
 					ACKNOWLEDGEDPACKETLIST.add(CURRENTACKNOWLEDGEDPACKETNUMBER - 1);
-					
-					  System.out.println(ACKNOWLEDGEDPACKETLIST.size() + "The size of ack list.");
-					  System.out.println(new BigInteger(sequenceNumberBytes).intValue() +
-					  ": Received number"); System.out.println(CURRENTACKNOWLEDGEDPACKETNUMBER +
-					  ": CurrentAckNumber");
-					 
+
+					System.out.println(ACKNOWLEDGEDPACKETLIST.size() + "The size of ack list.");
+					System.out.println(new BigInteger(sequenceNumberBytes).intValue() + ": Received number");
+					System.out.println(CURRENTACKNOWLEDGEDPACKETNUMBER + ": CurrentAckNumber");
+					// moving window pointer;
 					if (CURRENTACKNOWLEDGEDPACKETNUMBER > CURRENTWINDOWPOINTER) {
 						CURRENTWINDOWPOINTER = CURRENTACKNOWLEDGEDPACKETNUMBER;
 					}
@@ -127,9 +142,9 @@ public class ClientApp {
 			fileName = args[2];
 			windowSize = Integer.parseInt(args[3]);
 			MSS = Integer.parseInt(args[4]);
-			new SRRReceivePacket().start();
+			new SRRReceivePacket().start(); // init rece-packet thread
 			/* System.out.println("besides receiving thread"); */
-			new checkPacketTime().start();
+			new checkPacketTime().start(); // init packet-timer thread
 			SRRSendPacketToServer(serverHostName, serverPortNumber, fileName, windowSize, MSS);
 		} else {
 			System.out.println("Missing Command Line Arguments");
