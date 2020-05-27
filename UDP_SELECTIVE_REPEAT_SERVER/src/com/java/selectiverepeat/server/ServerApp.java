dc0ee2360fbd6c12ddfcd87809b5da8226921683
Ravi Rao <ravi.rao04@sap.com>
2020-05-27 20:21:30 +0530

    added comm

--- UDP_SELECTIVE_REPEAT_SERVER/src/com/java/selectiverepeat/server/ServerApp.java
+++ UDP_SELECTIVE_REPEAT_SERVER/src/com/java/selectiverepeat/server/ServerApp.java
@@ -1,13 +1,16 @@
 package com.java.selectiverepeat.server;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import com.java.selectiverepeat.server.entity.ServerPacket;
 import com.java.selectiverepeat.server.helper.ServerHelper;
@@ -36,10 +39,10 @@ public class ServerApp {
 		while (true) {
 			DatagramPacket dp = new DatagramPacket(b1, b1.length);
 			ds.receive(dp);
-			ServerPacket currentPacket = ServerHelper.decipherPacket(b1);
-			int sequenceNumber = currentPacket.getSequenceNumber();
+			ServerPacket currentPacket = ServerHelper.decipherPacket(b1); // deserializing
+			int sequenceNumber = currentPacket.getSequenceNumber(); // seq-no
 			// check if the correct packet is received or else dont send an acknowledgment
-
+			// Does pak-hist alread contains pak-seq then send duplicate pck
 			if (PacketHistory.contains(sequenceNumber)) {
 				System.out.println("Received Duplicate packet " + sequenceNumber);
 				byte[] acknowledgmentNumberBytes = ServerHelper.getAcknowledgmentNumber(sequenceNumber);
@@ -47,7 +50,9 @@ public class ServerApp {
 						dp.getAddress(), dp.getPort());
 				ds.send(dp2);
 			} else {
-
+				// else chech if the rec-pck-seq is with in expected window size
+				// eg : 50 <40 invalid
+				// eg o <1 ||5 < 9 : valid
 				if (sequenceNumber < expectedWindowSize) {
 					byte[] acknowledgmentNumberBytes = ServerHelper.getAcknowledgmentNumber(sequenceNumber);
 					DatagramPacket dp2 = new DatagramPacket(acknowledgmentNumberBytes, acknowledgmentNumberBytes.length,
@@ -55,18 +60,21 @@ public class ServerApp {
 					// TimeUnit.MILLISECONDS.sleep(50);
 					ds.send(dp2);
 					System.out.println("Packet No : " + sequenceNumber + "\t");
+					// packet stored with seq as key and data as value
 					currentWindowData.put(sequenceNumber, currentPacket);
+					// keeping track of pck-rec
 					PacketHistory.add(sequenceNumber);
 					/*
 					 * System.out.println("currentWindowdata" + currentWindowData.size()
 					 * +"window Size: "+ windowSize);
 					 */
+					// eg : 5-buffered >= 5- windowsize then write into file and clear the buffer
 					if (currentWindowData.size() >= windowSize) {
 						/* System.out.println("Inside if loop"); */
 						writeToFile();
 					}
 				} else {
-
+					// else drop packet without sending ack
 					System.out.println("Packet loss, sequence number:" + sequenceNumber);
 					expectedWindowSize = expectedWindowSize + windowSize;
 					TOTALPACKETLOSSCOUNT = TOTALPACKETLOSSCOUNT + 1;
